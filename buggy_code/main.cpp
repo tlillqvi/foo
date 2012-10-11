9afdffa8fa221c68f30bcff965e565cd2404a990
Marcus Eriksson <marcerik@gmail.com>
2012-10-11 14:18:17 +0300

    Kind of fixed it

--- buggy_code/main.cpp
+++ buggy_code/main.cpp
@@ -5,24 +5,33 @@ using namespace std;
 class Object
 {
 public:
-  int value;
-  Object(int v):value(v){}
+    int value;
+    Object(int v):value(v){}
 };
+
 
 class Factory
 {
 public:
-  Object& getObject()
-  {
-    Object o(20);
-    return o;
-  }
+    Object *o;
+    Object* getObject()
+    {
+        o=new Object(20);
+        return o;
+    }
+    
+    ~Factory()
+    {
+        cout << "Factory desctructor" << endl;
+        delete o;
+    }
 };
 
 
 int main()
 {
-  Factory f;
-  Object o = f.getObject();
-  cout << o.value << endl;
+    Factory f;
+    Object *o = f.getObject();
+    cout << o->value << endl;
 }
+
